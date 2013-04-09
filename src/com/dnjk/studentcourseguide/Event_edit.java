84666556acadc4383448577b4572615a5af77142
nikhs <nikhs.j@gmail.com>
2013-04-10 00:47:11 +0530

    calendar almost finished

--- src/com/dnjk/studentcourseguide/Event_edit.java
+++ src/com/dnjk/studentcourseguide/Event_edit.java
@@ -1,5 +1,7 @@
 package com.dnjk.studentcourseguide;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +9,8 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Event_edit extends Activity {
 Intent i=getIntent();
@@ -17,7 +21,10 @@ Intent i=getIntent();
 	
 	
 	}
-
+	
+	    
+	    
+	
 	public void setevent(View v){
 		EditText event=(EditText)findViewById(R.id.event_edit_name);
 		DatePicker datepick=(DatePicker) findViewById(R.id.event_pick_date);
@@ -26,7 +33,24 @@ Intent i=getIntent();
 		day=datepick.getDayOfMonth();
 		month=datepick.getMonth();
 		year=datepick.getYear();
+		String type;
+		int checkedId;
 		
+		RadioGroup Typeselect=(RadioGroup)findViewById(R.id.event_edit_type);
+		checkedId=Typeselect.getCheckedRadioButtonId();
+		
+		RadioButton rb=(RadioButton)findViewById(checkedId);
+		type=rb.getText().toString();
+		
+		Calendar cal = Calendar.getInstance();              
+		Intent intent = new Intent(Intent.ACTION_EDIT);
+		intent.setType("vnd.android.cursor.item/event");
+		intent.putExtra("beginTime", cal.getTimeInMillis());
+		intent.putExtra("allDay", false);
+		intent.putExtra("rrule", "FREQ=DAILY");
+		intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
+		intent.putExtra("title", name);
+		startActivity(intent);
 		
 	}
 	@Override
