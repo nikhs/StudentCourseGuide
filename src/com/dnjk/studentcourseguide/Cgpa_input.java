0c9f45f1cb75ae3fdcc6db6cc7f9beda35333106
Kumardev T B <tbkumardev@gmail.com>
2013-04-11 02:14:26 +0530

    rebuild cgpa part1

--- src/com/dnjk/studentcourseguide/Cgpa_input.java
+++ src/com/dnjk/studentcourseguide/Cgpa_input.java
@@ -1,11 +1,14 @@
 package com.dnjk.studentcourseguide;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class Cgpa_input extends Activity {
-
+Intent i=getIntent();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +21,19 @@ public class Cgpa_input extends Activity {
 		getMenuInflater().inflate(R.menu.cgpa_input, menu);
 		return true;
 	}
-
+	public void savecgpa(View v){
+		float sum,S12,S3,S4,S5,S6,S7,S8;
+		sum=S12=S3=S4=S5=S6=S7=S8=0;
+		S12=Float.valueOf(((EditText)findViewById(R.id.cgpa_input_12)).getText().toString());
+		S3=Float.valueOf(((EditText)findViewById(R.id.cgpa_input_3)).getText().toString());
+		S4=Float.valueOf(((EditText)findViewById(R.id.cgpa_input_4)).getText().toString());
+		S5=Float.valueOf(((EditText)findViewById(R.id.cgpa_input_5)).getText().toString());
+		S6=Float.valueOf(((EditText)findViewById(R.id.cgpa_input_6)).getText().toString());
+		S7=Float.valueOf(((EditText)findViewById(R.id.cgpa_input_7)).getText().toString());
+		S8=Float.valueOf(((EditText)findViewById(R.id.cgpa_input_8)).getText().toString());
+		sum=S12+S3+S4+S5+S6+S7+S8;
+		
+		
+		
+		}
 }
